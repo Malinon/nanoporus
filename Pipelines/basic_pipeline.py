9257d5d41e2f2359971df95ffa2e19598207b5d7
Jakub <jakubbmalinowski@gmail.com>
2023-12-16 19:53:32 +0100

    Update from laptop

--- Pipelines/basic_pipeline.py
+++ Pipelines/basic_pipeline.py
@@ -1,21 +1,36 @@
 from airflow import DAG
-from airflow.operators.bash import BashOperator
+from airflow.operators.python import PythonOperator
 from datetime import datetime, timedelta
+import joblib
+import sys
+import os.path
+NANPOR_REPO_PATH = "/home/xardas/Dokumenty/Nanoporous/nanoporus"
+sys.path.append(NANPOR_REPO_PATH  + '/Pipelines')
+
 import load_and_preprocess
 from cv_model_select import find_best_model_rf, find_best_model_lasso
-from report_generator import generate_report
+from report_generator import generate_report, plot_score_for_filtration
 from consts import *
 
-class ConeZFiltParams:
-    def __init__(self, radius, height):
-        self.radius = radius
-        self.height = height
+class CvResults:
+    def __init__(self, filt_params, is_lasso, use_yield):
+        self.filt_params = filt_params
+        self.is_lasso = is_lasso
+        self.use_yield = use_yield
+    def set_estim(self, estim):
+        self.estimator = estim
+    def get_name(self):
+        return "result_"+str(self.filt_params.radius) + "_" + str(self.filt_params.height) + str(self.is_lasso) + str(self.use_yield)
+    def dump(self):
+        name = self.get_name()
+        joblib.dump(self, name)
+        return name
 
 def get_data_file_name(filt_params, isTopo):
     if isTopo:
-        return "../Persistances/ConeFiltration/cone_" + str(filt_params.radius) + "_" + str(filt_params.height) + "_topo"
+        return NANPOR_REPO_PATH + "/Persistances/ConeFiltration/cone_" + str(filt_params.radius) + "_" + str(filt_params.height) + "_topo"
     else:
-        return "../Persistances/ConeFiltration/cone_" + str(filt_params.radius) + "_" + str(filt_params.height) + "_rand"
+        return NANPOR_REPO_PATH + "/Persistances/ConeFiltration/cone_" + str(filt_params.radius) + "_" + str(filt_params.height) + "_rand"
 
 def get_label_name(use_yield):
     if use_yield:
@@ -23,10 +38,10 @@ def get_label_name(use_yield):
     else:
         return "modulus"
 
-labels_path ="../"
+labels_path ="/home/xardas/Dokumenty/Nanoporous/nanoporus/"
 
-def otput_name_generator(filt_params):
-    return "data_" + str(filt_params.radius) + "_" + str(filt_params.height)
+def output_name_generator(filt_params):
+    return "/home/xardas/data_" + str(filt_params.radius) + "_" + str(filt_params.height)
 
 def prepare_data(filt_params):
     load_and_preprocess.prepare_data(filt_params, get_data_file_name, labels_path, output_name_generator)
@@ -39,59 +54,89 @@ filtrations_params = [ConeZFiltParams(3, 6),
 default_args = {
     'owner': 'airflow',
     'depends_on_past': False,
-    'start_date': datetime(2015, 6, 1),
+    'start_date': datetime(2023, 10, 24),
     'email': ['airflow@example.com'],
     'email_on_failure': False,
     'email_on_retry': False,
-    'retries': 1,
+    'retries': 0,
     'retry_delay': timedelta(minutes=5),
-    'schedule_interval': '@once',
+    'schedule_interval': None,
     # 'queue': 'bash_queue',
     # 'pool': 'backfill',
     # 'priority_weight': 10,
     # 'end_date': datetime(2016, 1, 1),
 }
 
-dag = DAG('tutorial', default_args=default_args)
+dag = DAG('pers_kub', default_args=default_args)
 
 def get_all_data_sets(filt_params):
-    return (joblib.load(), joblib.load(), joblib.load())
+    print("USed dataset ",output_name_generator(filt_params))
+    basic_name = output_name_generator(filt_params)# "/home/xardas/data_5_10"
+    return (joblib.load(basic_name), joblib.load(basic_name + "_topo"), joblib.load(basic_name + "_rand"))
 
 def find_best(filt_params, real_searcher, use_yield, is_lasso):
-    whole_data, topo_data, rand_data = 
-    return tuple((real_searcher(filt_params, data) for data in get_all_data_sets(filt_params)), filt_params, is_lasso)
+    whole_data, topo_data, rand_data = get_all_data_sets(filt_params)
+    cv_results = CvResults(filt_params, is_lasso, use_yield)
+    cv_resuls_name = cv_results.get_name()
+    if not os.path.isfile(cv_resuls_name):
+        cv_results.set_estim([real_searcher(splited_data=data, use_yield=use_yield) for data in get_all_data_sets(filt_params)])
+        joblib.dump(cv_results, cv_resuls_name)
+    else:
+        print("Skip")
+    return cv_resuls_name
 
 def create_search_ops(searcher, use_yield, is_lasso):
-    return [PythonOperator(task_id='search_cv_' + str(filt_params) "_" + get_label_name(use_yield) + str(is_lasso),
+    return [PythonOperator(task_id='search_cv_' + str(filt_params) + "_" + get_label_name(use_yield) + str(is_lasso), dag = dag,
                             python_callable = find_best, op_args=[filt_params, searcher, use_yield, is_lasso]) for filt_params in filtrations_params]
 
-load_data_ops = [PythonOperator(task_id='prepare_data_' + str(filt_params),
-                            python_callable = prepare_data, op_args=[filt_params]) for filt_params in filtrations_params]
-
-cv_search_yield = create_search_ops(find_best_model_rf, True, False) + create_search_ops(find_best_model_lasso, True, True)
-cv_search_modulus = create_search_ops(find_best_model_rf, False, False)+ create_search_ops(ind_best_model_lasso, False, True)
-
-generate_report_for_best_yield = PythonOperator(task_id='report_yield', python_callable = gen_rep_best_yields, provide_context=True)
-generate_report_for_best_modulus = PythonOperator(task_id='report_yield', python_callable = gen_rep_best_modulus, provide_context=True)
-
-def find_best_res(results, idx):
+def find_best_res(results, idx, filter_func=(lambda x: True)):
     max_score = 0.0
     best_mod = None
-    for res in results:
-        if res[0][idx].best_score_ > max_score:
-            best_mod = res[0][idx]
-            max_score = res[0][idx].best_score_
+    for res in filter(filter_func, results):
+        if res.estimator[idx].best_score_ > max_score:
+            best_mod = res
+            max_score = res.estimator[idx].best_score_
+    print("Max score", max_score, "Len: ", len(list(filter(filter_func, results))))
     return best_mod
 
-
-def gen_rep_best_yields(ti):
-    results = ti.xcom_pull(task_ids=[op.task_id for op in cv_search_yield])
+def gen_rep_best(ti, dependencies, name_score, use_yield=True):
+    results = [joblib.load(name) for name in ti.xcom_pull(task_ids=[op.task_id for op in dependencies])]
+    for rest in results:
+        print(rest.filt_params, " : ", rest.estimator[0].best_score_)
     best_models = [find_best_res(results, idx) for idx in range(3)]
     idx = 0
 
     for data_type in DataType:
-        datas = get_all_data_sets(best_models[idx][1])
-        generate_report(best_models[idx][0], datas[idx], True, data_type, best_models[idx][2])
+        datas = get_all_data_sets(best_models[idx].filt_params)
+        print("Best ", best_models[idx].filt_params, " ", str(data_type))
+        generate_report(best_models[idx].estimator[idx], datas[idx], use_yield, data_type, best_models[idx].is_lasso, best_models[idx].filt_params)
+        idx += 1
+    
+    best_for_filtration = []
+        
+    for filt in filtrations_params:
+        print(filt)
+        filter_func = lambda x: x.filt_params == filt
+        best_for_filtration.append(find_best_res(results, 0, filter_func).estimator[0].best_score_)
+    plot_score_for_filtration(best_for_filtration, filtrations_params, name_score)
 
-load_data_ops >> cv_search_yield  >> generate_report_for_best_yield
-load_data_ops >> cv_search_modulus >> generate_report_for_best_modulus
+# Define tasks
+
+load_data_ops = [PythonOperator(task_id='prepareDataa' + str(filt_params), dag = dag,
+                            python_callable = prepare_data, op_args=[filt_params]) for filt_params in filtrations_params]
+
+cv_search_yield = create_search_ops(find_best_model_rf, True, False) + create_search_ops(find_best_model_lasso, True, True)
+cv_search_modulus = create_search_ops(find_best_model_rf, False, False)+ create_search_ops(find_best_model_lasso, False, True)
+
+gen_rep_best_yields = lambda ti: gen_rep_best(ti, cv_search_yield, name_score="yield_score.png")
+gen_rep_best_modulus = lambda ti: gen_rep_best(ti, cv_search_modulus,  name_score="modulus_score.png", use_yield=False)
+
+generate_report_for_best_yield = PythonOperator(task_id='report_yield', python_callable = gen_rep_best_yields, provide_context=True, dag = dag)
+generate_report_for_best_modulus = PythonOperator(task_id='report_modulus', python_callable = gen_rep_best_modulus, provide_context=True, dag = dag)
+
+# Set dependencies
+for i in range(len(filtrations_params)):
+    load_data_ops[i] >> cv_search_yield[i]
+    load_data_ops[i] >> cv_search_modulus[i]
+cv_search_yield  >> generate_report_for_best_yield
+cv_search_modulus >> generate_report_for_best_modulus
