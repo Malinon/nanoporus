2eea681a0ab15c41375f6cbead0f2d403936722e
Jakub <jakubbmalinowski@gmail.com>
2024-01-31 17:58:20 +0100

    Adapt script for cone

--- gen_filtrations.jl
+++ gen_filtrations.jl
@@ -1,14 +1,33 @@
-include("weighted.jl")
+#include("weighted.jl")
+include("Julia/cone_filtrator/parser.jl")
 include("parser.jl")
+
 using DelimitedFiles
 
-function gen_out_name(i, z_multiplier, xy_multiplier, range)
-    return ("filtrations/pyramide_" * string(z_multiplier) * "_" * string(xy_multiplier) * "_" * string(range) * "/rand/filtration_" * string(i))
+function gen_out_name(i, radius, height, is_topo)
+    if is_topo
+        return ("filtrations/cone_" * string(radius) * "_" * string(height) * "/topo/filtration_" * string(i))
+    else
+        return ("filtrations/cone_" * string(radius) * "_" * string(height) * "/rand/filtration_" * string(i))
+    end
 end
 
-function gen_filtration(i, z_multiplier, xy_multiplier, range)
+HEIGHT = parse(Float64, ARGS[1])
+RADIUS = HEIGHT * parse(Float64, ARGS[2])
+FILTRATOR = WeightedConeFiltratorZ(RADIUS, (0.0, 0.0, HEIGHT))
+function filtrator(point, input_grid)
+    return call(FILTRATOR, point[1], point[2], point[3], input_grid)
+end
+
+
+
+const IS_TOPO = parse(Bool, ARGS[3])
+sample_num = IS_TOPO ? 110 : 75
+
+
+function gen_filtration(i, filtrator)
     println("Case " * string(i))
-    in_name = "../simulation_results/lmp_data_" * string(i)
+    in_name = IS_TOPO ? ("../topo_results/" * string(i) * ".lmp") :  ("../simulation_results/lmp_data_" * string(i))
     dt = read_material_from_file(in_name)
     sh = size(dt, 1)
     out = zeros(sh, sh, sh)
@@ -16,7 +35,7 @@ function gen_filtration(i, z_multiplier, xy_multiplier, range)
     for x in 1:sh
         for y in 1:sh
             for z in 1:sh
-                out[x, y, z] = get_weighted_pyramide_filtration((x, y, z), dt)
+                out[x, y, z] = filtrator((x, y, z), dt)
             end
         end
     end
@@ -25,8 +44,6 @@ function gen_filtration(i, z_multiplier, xy_multiplier, range)
 end
 
 
-
-
-for i in parse(Int64, ARGS[4]):74
-    gen_filtration(i, parse(Float64, ARGS[1]), parse(Float64, ARGS[2]), parse(Int64, ARGS[3]))
+for i in 0:(sample_num - 1)
+    gen_filtration(i, filtrator)
 end
--- persister.py
+++ persister.py
@@ -1,39 +1,60 @@
 """
 This script generates and saves persistence intervals of dim 0,1,2 based on filtration
 """
+print("PreStart")
 
 import numpy as np
 import joblib
 import gudhi as gd
 import gudhi.representations
-from datetime import datetime
+import numpy as np
 import sys
+import zipfile
+
+print("Start")
+HEIGHT = sys.argv[1]
+RADIUS = HEIGHT * sys.argv[2]
+IS_TOPO = sys.argv[2]
+
+SAMPLE_NUM = 110 if IS_TOPO else 75
 
 
-FILE_TMPL_IN = sys.argv[1]
-FILE_NAME_OUT = sys.argv[2]
-IS_TOPO = bool(sys.argv[3])
-EXCLUDE_TOPO = []
-if IS_TOPO:
-    EXCLUDE_TOPO =  [15, 20, 21, 25, 26, 30, 35, 40, 45, 50, 65, 66, 70]
+NAME = "cone_{radius}_{height}".format(radius = RADIUS, height= HEIGHT)
+DIR_NAME = "filtrations/" +NAME + ("/topo" if IS_TOPO else "/rand")
+OUT_NAME = "persistence_" + NAME + ("_topo" if IS_TOPO else "_rand")
 
-dgms = [[],[],[]]
+def gen_in_name(i):
+    return DIR_NAME + "/filtration_{i}".format(i=i)
+
+
+def read_filtration(file_name):
+    with open(file_name, 'r') as fp:
+        nums = [float(l) for l in fp.readlines()]
+        size = int((len(nums)) ** (1.0 / 3))
+        out_array = np.empty((size, size, size))
+        count=0
+        for z in range(size):
+            for y in range(size):
+                for x in range(size):
+                    out_array[x,y,z] = nums[count]
+                    count += 1
+        return out_array
 
 def get_persisistence_from_filtration_file(file_name):
-    filtr = joblib.load(file_name)
-    comp = gd.CubicalComplex(top_dimensional_cells=filtr)
-    now = datetime.now()
-    print("Start ", now.strftime("%H:%M:%S"))
+    filtr = read_filtration(file_name)
+    comp = gd.PeriodicCubicalComplex(top_dimensional_cells=filtr, periodic_dimensions=[False, False, True])
     comp.compute_persistence()
-    print("Pers ", now.strftime("%H:%M:%S"))
     return [comp.persistence_intervals_in_dimension(i) for i in range(3)]
 
+dgms = [[],[],[]]
+print("Start real")
+#with zipfile.ZipFile('filtrations/' + NAME + ".zip") as myzip:
+#    myzip.extractall()
+for i in range(SAMPLE_NUM):
+    print("case ", i)
+    file_name = gen_in_name(i)
+    pers = get_persisistence_from_filtration_file(file_name)
+    for j in range(3):
+        dgms[j].append(pers[j])
 
-for i in range(75):
-    if not i in EXCLUDE_TOPO:
-        file_name = FILE_TMPL_IN + str(i)
-        pers = get_persisistence_from_filtration_file(file_name)
-        for j in range(3):
-            dgms[j].append(pers[j])
-
-joblib.dump(dgms, FILE_NAME_OUT)
+joblib.dump(dgms, OUT_NAME)
